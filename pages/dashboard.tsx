721b02f5a95d8495a62e08247e5e40ab9c1300fc
armj <armandjermaks@gmail.com>
2023-11-04 18:00:21 +0200

    user dashboard base

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -4,24 +4,33 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
+    <>
       <Head>
         <title>Dashboard</title>
         <link rel="icon" href="/tab_icon.ico" />
       </Head>
-      <div className="flex flex-col pl-6 justify-evenly h-screen">
-        <Link href="/budget-recommendations">
-          <Button className="bg-slate-950 text-white mb-5">
-            Budžeta rekomendācijas
-          </Button>
-        </Link>
-        <Link href="/loan-calculator">
-          <Button className="bg-slate-950 text-white mb-5">Kredīta kalkulators</Button>
-        </Link>
-        <Link href="/tax-calculator">
-          <Button className="bg-slate-950 text-white">Nodokļu kalkulators</Button>
-        </Link>
+      <div className="bg-indigo-200">taskbar</div>
+      <div className="flex flex-row">
+        <div className="flex flex-col pl-6 justify-evenly basis-1/6 bg-lime-200">
+          <Link href="/budget-recommendations">
+            <Button className="bg-slate-950 text-white mb-5">
+              Budžeta rekomendācijas
+            </Button>
+          </Link>
+          <Link href="/loan-calculator">
+            <Button className="bg-slate-950 text-white mb-5">
+              Kredīta kalkulators
+            </Button>
+          </Link>
+          <Link href="/tax-calculator">
+            <Button className="bg-slate-950 text-white">
+              Nodokļu kalkulators
+            </Button>
+          </Link>
+        </div>
+        <div className="bg-amber-200 basis-3/6 ">statistics</div>
+        <div className="bg-red-200 basis-2/6 ">red flags</div>
       </div>
-    </div>
+    </>
   );
 }
