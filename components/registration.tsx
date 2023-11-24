cb2851bbd71e01cc5c92c72ce9a52adea07caf05
armj <armandjermaks@gmail.com>
2023-11-24 13:52:01 +0200

    adding pie chart / small changes

--- components/registration.tsx
+++ components/registration.tsx
@@ -1,5 +1,6 @@
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
+import { Input } from "./ui/input";
 
 interface RegistrationProps {
   onClose: () => void;
@@ -8,7 +9,11 @@ interface RegistrationProps {
 const RegistrationCard = (props: RegistrationProps) => {
   return (
     <Card className="flex flex-col shadow-lg rounded p-4">
-      <p>hello world!</p>
+      <p>Ievadiet pieprasītos datus</p>
+      <Input className="mb-3" type="text" placeholder="First Name" />
+      <Input className="mb-3" type="text" placeholder="Last Name" />
+      <Input className="mb-3" type="email" placeholder="Email" />
+      <Input className="mb-3" type="password" placeholder="Password" />
       <Button className="bg-white text-black" onClick={props.onClose}>
         Close
       </Button>
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Example from "@/components/pie-chart";
+import StatisticsPieChart from "@/components/pie-chart";
 
 export default function Home() {
   return (
@@ -10,8 +12,10 @@ export default function Home() {
         <link rel="icon" href="/tab_icon.ico" />
       </Head>
       <div className="flex flex-row bg-indigo-200 p-2">
-        <img src="/profile-circle.svg" width="50px"/>
-        <p className="pl-2">Labdien, <b>lietotāj!</b></p>
+        <img src="/profile-circle.svg" width="50px" />
+        <p className="pl-2">
+          Labdien, <b>lietotāj!</b>
+        </p>
       </div>
       <div className="flex flex-row">
         <div className="flex flex-col p-6 justify-evenly basis-1/6 bg-lime-200">
@@ -31,7 +35,10 @@ export default function Home() {
             </Button>
           </Link>
         </div>
-        <div className="bg-amber-200 basis-3/6 p-6">statistics</div>
+        <div className="bg-amber-200 basis-3/6 p-6">
+          <p>statistics</p>
+          <StatisticsPieChart />
+        </div>
         <div className="bg-red-200 basis-2/6 p-6">red flags</div>
       </div>
     </>
