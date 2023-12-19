37248f1dc6d1279aa05a24a7516d496c0dd58959
armands <armandjermaks@gmail.com>
2023-12-19 17:16:16 +0200

    adding signout button

--- pages/dashboard/add-initial-data.tsx
+++ pages/dashboard/add-initial-data.tsx
@@ -1,22 +1,29 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { signOut } from "next-auth/react";
 import Head from "next/head";
 
 const AddInitialData = (data: any) => {
   return (
-    <>
+    <div className="bg-green-200 h-screen">
       <Head>
         <title>Add initial data</title>
         <link rel="icon" href="/tab_icon.ico" />
       </Head>
-      <div className="flex flex-col items-center bg-green-200 h-screen">
-        <h1 className="mt-5 text-xl">
-          Paldies par reģistrāciju, <b>{data.firstName}</b>!
-        </h1>
-        <p className="text-l mb-5">
-          Pirms rīka izmantošanas, lūdzu, ievadiet zemāk prasīto informāciju
-        </p>
+      <div className="flex flex-col items-center">
+        <div className="flex flex-row justify-between w-full pr-5">
+          <div className="invisible">cccccccccccccccc</div>
+          <div className="flex flex-col items-center">
+            <h1 className="mt-5 text-xl">
+              Paldies par reģistrāciju, <b>{data.firstName}</b>!
+            </h1>
+            <p className="text-l mb-5">
+              Pirms rīka izmantošanas, lūdzu, ievadiet zemāk prasīto informāciju
+            </p>
+          </div>
+          <Button className="flex mt-7 ml-50" onClick={() => signOut({ callbackUrl: "/" })}>Atteikties</Button>
+        </div>
         <Card className="flex flex-col p-10">
           <div className="flex flex-row pb-5">
             <div className="pr-10">
@@ -27,11 +34,17 @@ const AddInitialData = (data: any) => {
               <Input className="mb-2" placeholder="Mājoklis" />
               <Input className="mb-2" placeholder="Transports" />
               <Input className="mb-2" placeholder="Pārtika" />
-              <Input className="mb-2 mr-3" placeholder="Veselība / skaistumkopšana" />
+              <Input
+                className="mb-2 mr-3"
+                placeholder="Veselība / skaistumkopšana"
+              />
               <Input className="mb-2" placeholder="Bērni" />
             </div>
             <div>
-              <Input className="mt-8 mb-2" placeholder="Iepirkšanās / pakalpojumi" />
+              <Input
+                className="mt-8 mb-2"
+                placeholder="Iepirkšanās / pakalpojumi"
+              />
               <Input className="mb-2" placeholder="Brīvais laiks / izklaide" />
               <Input className="mb-2" placeholder="Izglītība" />
               <Input className="mb-2" placeholder="Atpūta" />
@@ -45,7 +58,7 @@ const AddInitialData = (data: any) => {
           <Button>Iesniegt</Button>
         </Card>
       </div>
-    </>
+    </div>
   );
 };
 export default AddInitialData;
