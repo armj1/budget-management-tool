364fe1f323272c36a399d7dba61388ea8ba657cb
armands <armandjermaks@gmail.com>
2023-12-30 21:57:30 +0200

    budget recommendations page

--- pages/dashboard/budget-recommendations.tsx
+++ pages/dashboard/budget-recommendations.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import FinancialRecord from "@/interfaces/FinancialRecord";
+import { Dot } from "lucide-react";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 
@@ -30,6 +31,126 @@ const BudgetRecommendations = () => {
     setSelectedReport(report);
   };
 
+  const totalSpending =
+    (selectedReport?.childSpending ?? 0) +
+    (selectedReport?.educationSpending ?? 0) +
+    (selectedReport?.foodSpending ?? 0) +
+    (selectedReport?.healthSpending ?? 0) +
+    (selectedReport?.housingSpending ?? 0) +
+    (selectedReport?.insuranceSpending ?? 0) +
+    (selectedReport?.investmentSpending ?? 0) +
+    (selectedReport?.leisureSpending ?? 0) +
+    (selectedReport?.otherSpending ?? 0) +
+    (selectedReport?.petSpending ?? 0) +
+    (selectedReport?.recreationSpending ?? 0) +
+    (selectedReport?.shoppingSpending ?? 0) +
+    (selectedReport?.transportSpending ?? 0);
+
+  const leftoverMoney = Math.max(parseFloat(((selectedReport?.taxedIncome ?? 0) - totalSpending).toFixed(2)), 0);
+
+  const [recommendationExists, setRecommendationExists] = useState(true);
+  const recommendationCalculations = () => {
+    let isRecommendation = true;
+    let recommendedHousing = 0;
+    let recommendedSavings = 0;
+    let recommendedFood = 0;
+    let recommendedInsurance = 0;
+    let recommendedTransport = 0;
+    let recommendedHealth = 0;
+    let recommendedLeisure = 0;
+    let openHousingRecomm = false;
+    let openSavingsRecomm = false;
+    let openLeftoverZero = false;
+    let openLeftover = false;
+    let openFood = false;
+    let openInsurance = false;
+    let openTransport = false;
+    let openHealth = false;
+    let openLeisure = false;
+    const netIncome = selectedReport?.taxedIncome ?? 0;
+    let anyConditionMet = false;
+
+    if ((selectedReport?.housingSpending ?? 0) > netIncome * 0.3) {
+      recommendedHousing = Math.round(netIncome * 0.3 * 100) / 100;
+      openHousingRecomm = true;
+      anyConditionMet = true;
+    }
+
+    if ((selectedReport?.investmentSpending ?? 0) < netIncome * 0.15) {
+      recommendedSavings = Math.round(netIncome * 0.15 * 100) / 100;
+      openSavingsRecomm = true;
+      anyConditionMet = true;
+    }
+
+    if (leftoverMoney == 0) {
+      openLeftoverZero = true;
+      anyConditionMet = true;
+    } else if (leftoverMoney > 0) {
+      openLeftover = true;
+      anyConditionMet = true;
+    }
+
+    if ((selectedReport?.foodSpending ?? 0) > netIncome * 0.15) {
+      recommendedFood = Math.round(netIncome * 0.15 * 100) / 100;
+      openFood = true;
+      anyConditionMet = true;
+    }
+
+    if ((selectedReport?.insuranceSpending ?? 0) > netIncome * 0.25) {
+      recommendedInsurance = Math.round(netIncome * 0.25 * 100) / 100;
+      openInsurance = true;
+      anyConditionMet = true;
+    }
+
+    if ((selectedReport?.transportSpending ?? 0) > netIncome * 0.15) {
+      recommendedTransport = Math.round(netIncome * 0.15 * 100) / 100;
+      openTransport = true;
+      anyConditionMet = true;
+    }
+
+    if ((selectedReport?.healthSpending ?? 0) > netIncome * 0.1) {
+      recommendedHealth = Math.round(netIncome * 0.1 * 100) / 100;
+      openHealth = true;
+      anyConditionMet = true;
+    }
+
+    if ((selectedReport?.leisureSpending ?? 0) > netIncome * 0.1) {
+      recommendedLeisure = Math.round(netIncome * 0.1 * 100) / 100;
+      openLeisure = true;
+      anyConditionMet = true;
+    }
+
+    if (!anyConditionMet) {
+      isRecommendation = false;
+    }
+
+    return {
+      isRecommendation: isRecommendation,
+      housing: recommendedHousing,
+      savings: recommendedSavings,
+      food: recommendedFood,
+      insurance: recommendedInsurance,
+      transport: recommendedTransport,
+      health: recommendedHealth,
+      leisure: recommendedLeisure,
+      openHousingRecomm: openHousingRecomm,
+      openSavingsRecomm: openSavingsRecomm,
+      openLeftoverZero: openLeftoverZero,
+      openLeftover: openLeftover,
+      openFood: openFood,
+      openInsurance: openInsurance,
+      openTransport: openTransport,
+      openHealth: openHealth,
+      openLeisure: openLeisure,
+    };
+  };
+
+  const recommendations = recommendationCalculations();
+
+  useEffect(() => {
+    setRecommendationExists(recommendations.isRecommendation);
+  }, [recommendations.isRecommendation]);
+
   return (
     <NavbarLayout>
       <Head>
@@ -55,11 +176,67 @@ const BudgetRecommendations = () => {
         </div>
         <Card className="p-10">
           {!selectedReport && <p className="text-lg">Nav atskaites - nav padomu!</p>}
-          {selectedReport && (
-            <div>
+          {selectedReport && recommendationExists && (
+            <div className="flex flex-col">
               <p className="text-lg">Balstoties uz atskaites "{selectedReport?.title}" datiem mums ir radušies šādi ieteikumi:</p>
+              {recommendations.openHousingRecomm && (
+                <p className="flex flex-row">
+                  <Dot />
+                  Jūsu izdevumi mājoklim pārsniedz ieteicamos 30% no neto ienākuma - vēlamie maksimālie mājokļa izdevumi ir €{recommendations.housing}, pašlaik
+                  tie ir €{selectedReport?.housingSpending}
+                </p>
+              )}
+              {recommendations.openSavingsRecomm && (
+                <p className="flex flex-row">
+                  <Dot />
+                  Jūsu izdevumi uzkrājumiem nesasniedz ieteicamos 15% no neto ienākumiem - būtu vēlams tiem atvēlēt ap €{recommendations.savings}, pašlaik tie
+                  ir €{selectedReport?.investmentSpending}
+                </p>
+              )}
+              {recommendations.openLeftover && (
+                <p className="flex flex-row">
+                  <Dot /> Jums ir radies atlikums €{leftoverMoney} apmērā - Jūs varat to ieguldīt uzkrājumos un palielināt tos līdz €
+                  {(selectedReport?.investmentSpending ?? 0) + leftoverMoney}
+                </p>
+              )}
+              {recommendations.openLeftoverZero && (
+                <p className="flex flex-row">
+                  <Dot /> Jums nav radies budžeta atlikums - iesakām pārskatīt izdevumus
+                </p>
+              )}
+              {recommendations.openFood && (
+                <p className="flex flex-row">
+                  <Dot /> Jūsu izdevumi pārtikai pārsniedz ieteicamos 15% no neto ienākuma - vēlamie maksimālie pārtikas izdevumi ir €{recommendations.food},
+                  pašlaik tie ir €{selectedReport?.foodSpending}
+                </p>
+              )}
+              {recommendations.openInsurance && (
+                <p className="flex flex-row">
+                  <Dot /> Jūsu izdevumi apdrošināšanai pārsniedz 25% no neto ienākuma - vēlamie maksimālie apdrošināšanas izdevumi ir €
+                  {recommendations.insurance}, pašlaik tie ir €{selectedReport?.insuranceSpending}
+                </p>
+              )}
+              {recommendations.openTransport && (
+                <p className="flex flex-row">
+                  <Dot /> Jūsu izdevumi transportam pārsniedz 15% no neto ienākuma - vēlamie maksimālie transporta izdevumi ir €{recommendations.transport},
+                  pašlaik tie ir €{selectedReport?.transportSpending}
+                </p>
+              )}
+              {recommendations.openHealth && (
+                <p className="flex flex-row">
+                  <Dot /> Jūsu izdevumi veselībai un skaistumkopšanai pārsniedz 10% no neto ienākuma - vēlamie maksimālie izdevumi ir €{recommendations.health},
+                  pašlaik tie ir €{selectedReport?.healthSpending}
+                </p>
+              )}
+              {recommendations.openLeisure && (
+                <p className="flex flex-row">
+                  <Dot /> Jūsu izdevumi izklaidei pārsniedz 10% no neto ienākuma - vēlamie maksimālie izklaides izdevumi ir €{recommendations.leisure},
+                  pašlaik tie ir €{selectedReport?.leisureSpending}
+                </p>
+              )}
             </div>
           )}
+          {!recommendationExists && selectedReport && <p className="text-lg">Atskaitei "{selectedReport?.title}" nav radušies ieteikumi</p>}
         </Card>
       </div>
     </NavbarLayout>
